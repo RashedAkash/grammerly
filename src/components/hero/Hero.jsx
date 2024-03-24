c0f6f42887f44ec945e12061f25c7e19dc7acea3
hablu <131282586+hablu@users.noreply.github.com>
2024-03-24 17:10:46 +0600

    hero section responsive complete

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -19,21 +19,30 @@ const Hero = () => {
               </p>
 
               <div className="flex items-center">
-                <button className="w-full px-5 py-5 mt-6 text-sm  text-white  transition-colors duration-300 transform bg-[#027e6f] rounded-lg lg:w-auto ">
-                  Sign up It's free
-                </button>
-                <button className="w-full ml-3 px-5 py-5 mt-6 text-sm  text-[#0e101a] border border-[#0e101a]   transition-colors duration-300 rounded-lg lg:w-auto inline-block   ">
-                  <div className="flex items-center">
-                    <Image src="/gogle.png" alt="img" width={16} height={16} />
-                    <span className=" ml-2">Sign up with google</span>
-                  </div>
-                </button>
+                <div>
+                  <button className="w-full px-5 py-5 mt-6 text-sm  text-white  transition-colors duration-300 transform bg-[#027e6f] rounded-lg lg:w-auto ">
+                    Sign up It's free
+                  </button>
+                </div>
+                <div>
+                  <button className="w-full ml-3 px-5 py-5 mt-6 text-sm  text-[#0e101a] border border-[#0e101a]   transition-colors duration-300 rounded-lg lg:w-auto inline-block   ">
+                    <div className="flex items-center">
+                      <Image
+                        src="/gogle.png"
+                        alt="img"
+                        width={16}
+                        height={16}
+                      />
+                      <span className=" ml-2">Sign up with google</span>
+                    </div>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
 
           <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
-            <video width="640" height="360" controls>
+            <video width="640" height="360" autoPlay muted loop >
               <source
                 src="https://static-web.grammarly.com/1e6ajr2k4140/5EqhPyvNiIygTb5ZNekZOu/c9b6f09c33898142415eac7ed465bd7b/013124_Hero_Full_2x_NewLogo_1240x784.mp4"
                 type="video/mp4"
